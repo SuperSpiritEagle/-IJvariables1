3534f59d169b37fddbbede1f5f053fb5576889af
SuperSpiritEagle <superspiriteagle@gmail.com>
2022-12-03 11:44:32 +0500

    init

---
+++ Variables1/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Variables1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string surname = "Jonson";
+            string name = "Jon";
+            string patronymic = "Johnsovich";
+            short age = 25;
+
+            bool snowIsFalling = true;
+            bool alive = true;
+            bool isDead = false;
+
+            const byte yourBirthday = 28;
+
+            sbyte airTtemperatureOnEarth = -22;
+
+            float theSumOftTheDivision = yourBirthday / (float)age;
+
+        }
+    }
+}
